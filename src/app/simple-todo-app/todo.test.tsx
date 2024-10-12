2702a5c60068fd1583881659f4214054c18f9356
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2024-10-12 23:06:10 +0530

    till here every thing is working

--- src/app/simple-todo-app/todo.test.tsx
+++ src/app/simple-todo-app/todo.test.tsx
@@ -1,9 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import TodoApp from "./page";
 
-describe("<App />", () => {
-  it("renders", () => {
-    render(<TodoApp />);
-    expect(true).toBeTruthy();
-  });
+describe("test to do application", () => {
+    beforeAll(() => {
+        render(<TodoApp />);
+    });
+
+    it("should render", () => {
+        expect(document.querySelector(".input")).toBeVisible();
+    });
+
+    it("should add a todo item", () => {
+        const text = "Walk the dog to todo item";
+        const input = screen.getByTestId("todo-input");
+
+        // Simulate input change
+        fireEvent.change(input, { target: { value: text } });
+        expect(input).toHaveValue(text);
+    });
 });
